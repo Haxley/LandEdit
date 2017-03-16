ad791056c079bcdb6cb99cc772dd17a1ec34d11e
Haxley <plooms@plooms.com>
2017-03-15 19:00:46 -0700

    Now Dumps to Land.yml
    The edited data now writes to the Land.YML file but not yet in the
    correct YAML format.

--- landedit.php
+++ landedit.php
@@ -10,18 +10,20 @@
 include('/Users/keithdavis/MineCraft/pocketmine-mp/plugins/EconomyLand/Spyc.php');
 
 #Loads Land.yml into PHP Array
-$array = Spyc::YAMLLoad('/Users/keithdavis/MineCraft/pocketmine-mp/plugins/EconomyLand/Land.yml');
+$array = Spyc::YAMLLoad('/Users/keithdavis/Documents/GitHub/LandEdit/Land.yml');
+
+$file = 'Land.yml';
 
 echo "Loaded into PHP: \n";
 print_r($array);
 
-$answerOne = readline("Would you like to
+echo "Would you like to
 \n1: Delete all land claims by owner?
-\n2: Delete all land claims except owner?
-\nType 1 or 2:");
+\n2: Delete all land claims except owner?\n";
+$answer = readline("\nType 1 or 2: ");
 
 #Answer 1
-if ($answerOne == 1){
+if ($answer == 1){
   $ownerName = readline("What is the owner's name?:  ");
 
   #Searches land array for owner
@@ -34,6 +36,7 @@ if ($answerOne == 1){
 
       #Sends data back to Land.yml
       $landAfterDump = Spyc::YAMLDump($array);
+      file_put_contents($file, $landAfterDump);
       echo "YAML Data dumped back: \n";
       echo $landAfterDump;
 
@@ -41,7 +44,7 @@ if ($answerOne == 1){
       echo 'Did not find ' . $ownerName . "\n";
   }
 #Answer 2
-}else{
+}elseif ($answer == 2){
   $ownerName = readline("What is the owner's name?:  ");
 
   #Searches land array for owner
@@ -61,6 +64,8 @@ if ($answerOne == 1){
   else {
         echo 'Did not find ' . $ownerName . "\n";
   }
+}else {
+  echo "Input not recognized. Please run program again.\n";
 }
 
 
